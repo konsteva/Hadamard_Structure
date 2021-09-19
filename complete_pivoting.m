f1d0d62c814ec54bf469c71efa405de08274be0f
Eva K <58198596+konsteva@users.noreply.github.com>
2021-09-19 15:39:56 +0300

    Update complete_pivoting.m

--- complete_pivoting.m
+++ complete_pivoting.m
@@ -5,35 +5,39 @@ function [L,U,P,Q] = complete_pivoting(A)
   P = eye(n) ;
   Q = eye(n) ;
   for k = 1:n
-    % αποθηκευει το μεγαλυτερο στοιχειο καθε στηλης απο τη γραμμη και τη στηλη k και 
-    % κατω με τη μορφη [στοιχειο, αριθμος γραμμης]
+  
+    % stores the greatest element of each column, for every column and row > k in the form [element, row number]
     [m_c,r] = max(abs(A(k:n,k:n)));
-    % αποθηκευει το max των προηγουμενων στοιχειων
-    % με τη μορφη [στοιχειο, αριθμος στηλης]
+    
+    % stores max element of the above in the form [element, column number]
     [m,c] = max(m_c);
     r = r(c)+k-1; 
     c = c+k-1;
-    %Εναλλαγες γραμμων
+    
+    % Interchange of rows
     A([k r],:) = A([r k],:);
     P([k r],:) = P([r k],:);
     L([k r],:) = L([r k],:);
-    %Εναλλαγες στηλων
+    
+    % Interchange of columns
     A(:,[k c]) = A(:,[c k]);
     L(:,[k c]) = L(:,[c k]);
     Q(:,[k c]) = Q(:,[c k]);
-    %Ελεγχος αν ο πινακας ειναι ιδιαζων
+    
+    % Check if matrix is singular
     if A(k,k) == 0
       fprintf('Matrix is singular. Cannot complete factorization') ;
       break
     end
-    % Κατασκευαζουμε τον L διαιρωντας τα στοιχεια της στηλης k απο την
-    % γραμμη k και κατω με το pivot
+    
+    % Construct matrix L by dividing the elements in column k for rows > k with the pivot
     format rat
     L(k:n,k) = A(k:n,k) / A(k,k);
-    % Κατασκευαζουμε τον U κανοντας overwrite τις γραμμες του με τις 
-    % μετασχηματισμενες του πινακα Α
+    
+    % Construct matrix U by overwritting its rows with the transformed ones of the matrix A
     U(k,1:n) = A(k,1:n);
-    %Μετασχηματισμος του Α
+    
+    % Matrix A transformation
     A(k+1:n,1:n) = A(k+1:n,1:n) - L(k+1:n,k)*A(k,1:n);
   end
 end
