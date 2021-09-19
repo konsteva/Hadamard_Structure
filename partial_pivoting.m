5f5847d371813bde0bb96db78cfce4752f85c3d8
Eva K <58198596+konsteva@users.noreply.github.com>
2021-09-19 15:42:46 +0300

    Update partial_pivoting.m

--- partial_pivoting.m
+++ partial_pivoting.m
@@ -4,27 +4,30 @@ function [L,U,P] = partial_pivoting(A)
   U = zeros(n);
   P = eye(n);
   for k=1:n
-    % αποθηκευει το μεγαλυτερο στοιχειο της στηλης k απο τη γραμμη k και κατω
-    % με τη μορφη [στοιχειο, αριθμος γραμμης]
+  
+    % stores the greatest element of each column, for every column and row > k in the form [element, row number]
     [m,r] = max(abs(A(k:n,k)));
     r = r+k-1;    
-    %Εναλλαγες γραμμων
+    
+    % Interchange of rows
     A([k r],:) = A([r k],:);
     P([k r],:) = P([r k],:);
     L([k r],:) = L([r k],:);
-    %Ελεγχος αν ο πινακας ειναι ιδιαζων
+    
+    % Check if the matrix is Singular
     if A(k,k) == 0
       fprintf('Matrix is singular. Cannot complete factorization') ;
       break
     end
-    % Κατασκευαζουμε τον L διαιρωντας τα στοιχεια της στηλης k απο την
-    % γραμμη k και κατω με το pivot (L_ik = a_{kk}/a_{kk} , a_{k+1 k}/a_{kk}, ... )  
+    
+    % Construct matrix L by dividing the elements in column k for rows > k with the pivot (L_ik = a_{kk}/a_{kk} , a_{k+1 k}/a_{kk}, ... )  
     format rat
-    L(k:n,k) = A(k:n,k) / A(k,k) 
-    % Κατασκευαζουμε τον U κανοντας overwrite τις γραμμες του με τις 
-    % μετασχηματισμενες του πινακα Α
+    L(k:n,k) = A(k:n,k) / A(k,k)
+    
+    % Construct matrix U by overwritting its rows with the transformed ones of the matrix A
     U(k,1:n) = A(k,1:n);
-    %Μετασχηματισμος του Α
+    
+    % Transform matrix A
     A(k+1:n,1:n) = A(k+1:n,1:n) - L(k+1:n,k)*A(k,1:n);  
   end
   U(:,n) = A(:,n);
